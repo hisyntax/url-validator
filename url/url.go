6745a4ee81a8d22e702e6a3d679e19c81661dc67
hisyntax <hisyntax0@gmail.com>
2022-11-21 17:13:51 +0100

    url and text string added to response

--- url/url.go
+++ url/url.go
@@ -28,31 +28,29 @@ func getTld() ([]string, error) {
 	return arr, nil
 }
 
-func ValidateURL(url string) error {
+func ValidateURL(url string) (string, error) {
 	//send a get request to get the list of all tld
 	arr, err := getTld()
 	if err != nil {
 		fmt.Printf("This is the error from the request: %v", err)
-		return err
+		return "", err
 	}
 	//loop through to get a match with the url provided
 	for _, v := range arr {
 		tld1 := fmt.Sprintf(" .%s", v)
 		tld2 := fmt.Sprintf(".%s", v)
 		if strings.Contains(url, tld1) {
-			return errors.New("invalid url")
-
+			return "url", errors.New("invalid url")
 		} else if strings.Contains(url, tld2) {
 			// send a request to the url provided to confirm if valid or not
 			valid, _, err := request.NewRequest("GET", url, "")
 			if err != nil {
-				return errors.New("request url invalid")
+				return "", errors.New("request url invalid")
 			}
 			fmt.Printf("This is the request url response: %v\n", string(valid))
 			// return response
-
-			return nil
+			return "url", nil
 		}
 	}
-	return nil
+	return "text", nil
 }
